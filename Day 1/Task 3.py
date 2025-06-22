7a44ab989385870595a10408872285f632d447b7
Shresth Mishra <shresthmishr@gmail.com>
2025-06-22 23:51:54 +0530

    Task 3 added

---
+++ Day 1/Task 3.py
@@ -0,0 +1,16 @@
+# Task 3: Inputs
+
+'''
+Learn to use the Python input() function to collect user input and use it within your code.
+
+PAUSE 1. Update the code to add an exclamation mark Using what you have learnt in this lesson and previous, can you figure out how take user input and slot it in between 2 strings?
+
+You are looking to print a sentence like this:
+
+Hello Name!
+
+e.g. Hello Angela!
+'''
+
+print("My name is" + " " + "Angela")
+print("Hello " + input("What is your name? ") + "!")
