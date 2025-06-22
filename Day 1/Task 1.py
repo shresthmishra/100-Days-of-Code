d4f9ae72b692eb86ece94f76d909051d6248d1c8
Shresth Mishra <shresthmishr@gmail.com>
2025-06-22 23:20:09 +0530

    First day!

---
+++ Day 1/Task 1.py
@@ -0,0 +1,6 @@
+# Description
+# Use what you learnt to print out the words "Hello world!" with Python code.
+# Then click the run button to execute your code.
+
+# Write your code below this line 👇
+print("Hello there worldie!")
