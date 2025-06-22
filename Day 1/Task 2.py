c3351e0b2f527c947c38197a66517aa88569d9b5
Shresth Mishra <shresthmishr@gmail.com>
2025-06-22 23:49:39 +0530

    Task 2 addition

---
+++ Day 1/Task 2.py
@@ -0,0 +1,25 @@
+# Task 2: String Manipulation
+
+'''
+Learn to use string concatenation and the new line escape sequence to format strings in Python.
+
+PAUSE 1. Use \n to add another line of "Hello world".
+So the resulting output looks like this:
+
+Hello world!
+
+Hello world!
+
+Hello world!
+
+PAUSE 2. Add a space between the strings
+So there is a space between the string Hello and Angela when the print statement runs.
+
+The output should look like this:
+
+Hello Angela
+'''
+
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+print("Hello " + "Angela")
