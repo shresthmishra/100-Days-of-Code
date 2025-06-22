98e0bec366a6574a9751bfd1b668b1e3cc513c9c
Shresth Mishra <shresthmishr@gmail.com>
2025-06-22 23:49:09 +0530

    Fixed Task 1's format

--- Day 1/Task 1.py
+++ Day 1/Task 1.py
@@ -1,6 +1,9 @@
 # Task 1: Printing
-# Use what you learnt to print out the words "Hello world!" with Python code.
-# Then click the run button to execute your code.
+
+'''
+Use what you learnt to print out the words "Hello world!" with Python code.
+Then click the run button to execute your code.
+'''
 
 # Write your code below this line 👇
 print("Hello there worldie!")
