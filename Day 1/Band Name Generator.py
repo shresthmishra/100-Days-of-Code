a47538e5aafcbf414d626d41344bac74ea82bc81
Shresth Mishra <shresthmishr@gmail.com>
2025-06-23 00:03:21 +0530

    Day 1 project completed

---
+++ Day 1/Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Shresth's Band Name Generator!\n")
+city = input("What's the name of the city that you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your band name could be " + city + " " + pet)
