fc470609d015a3b37c424b1c4c39767030607d38
Shresth Mishra <shresthmishr@gmail.com>
2025-06-29 19:16:53 +0530

    Code Task 3

---
+++ Day 2/Task 3.py
@@ -0,0 +1,21 @@
+# Mathematical Operations
+
+'''
+# Basic Operators
+Learn to use the basic mathematical operators, +, -, *, /, // and **
+
+# PEMDAS
+Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+Task 2.1: What is the output of this code?
+print(3 * 3 + 3 / 3 - 3)
+
+Task 2.2: Change the code so it outputs 3?
+print(3 * 3 + 3 / 3 - 3)
+'''
+
+print("My age: " + str(22))
+# PEMDASLR
+print(3 * 3 + 3 / 3 - 3)
+
+print(3 * (3 + 3) / 3 - 3)
