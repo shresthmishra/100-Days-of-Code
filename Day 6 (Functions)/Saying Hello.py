04a2e29262688846e0238948a01d96c989dfd995
Shresth Mishra <shresthmishr@gmail.com>
2025-07-09 00:01:13 +0530

    Code Function example

--- Day 6 (Functions)/Saying Hello.py
+++ Day 6 (Functions)/Saying Hello.py
@@ -1,13 +1,13 @@
 # Functions in Python
 
-'''
+"""
 In Python, functions are blocks of organized, reusable code designed to perform a specific, related action. 
 They are fundamental to structuring programs effectively, promoting code reusability, and enhancing readability.
 
 # Defining a Function:
 Functions are defined using the def keyword, followed by the function name, parentheses that may contain parameters, and a colon. 
 The function's body is indented below the line.
-'''
+"""
 
 def my_function():
     print("Hello, there")
