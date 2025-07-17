a90a879ab586d8b61c6a4d2cb3350c97928ffe55
Shresth Mishra <shresthmishr@gmail.com>
2025-07-17 12:11:09 +0530

    Code Function calling example

---
+++ Day 6 (Functions)/Saying Hello and Users Name.py
@@ -0,0 +1,14 @@
+""""
+# Calling a Function
+Calling a function just means triggering the function. We can call a function at any point in our code in Python.
+
+<function name>()
+Putting everything together e.g.
+"""
+
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+
+print("Hello")
+get_user_name() # Calling the function
