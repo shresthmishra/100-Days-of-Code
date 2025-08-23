2f28f9fb5cab4016b771180d9301e30d9bd1eec5
Shresth Mishra <shresthmishr@gmail.com>
2025-08-24 01:08:25 +0530

    Code Step 2 of Day 7

---
+++ Day 7 (Hangman)/Step 2.py
@@ -0,0 +1,22 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+
+guess = input("Guess a letter: ").lower()
+placeholder = ""
+for _ in range(len(chosen_word)):
+    placeholder += "_"
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
+display = ""
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+    else:
+        display += "_"
+print(display)
