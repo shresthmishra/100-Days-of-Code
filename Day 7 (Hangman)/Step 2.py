19ea948e7e5032371a953b9c000a44793aca6e38
Shresth Mishra <shresthmishr@gmail.com>
2025-08-24 01:14:35 +0530

    Update Step 2 of Day 7

--- Day 7 (Hangman)/Step 2.py
+++ Day 7 (Hangman)/Step 2.py
@@ -7,7 +7,9 @@ print(chosen_word)
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
 
 guess = input("Guess a letter: ").lower()
+
 placeholder = ""
+
 for _ in range(len(chosen_word)):
     placeholder += "_"
 print(placeholder)
@@ -15,9 +17,11 @@ print(placeholder)
 # TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
 
 display = ""
+
 for letter in chosen_word:
     if letter == guess:
         display += letter
     else:
         display += "_"
+        
 print(display)
