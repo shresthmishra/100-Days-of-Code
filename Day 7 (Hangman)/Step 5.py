930a8ad3ca64d84d08be3b42f1075280ccac8a6f
Shresth Mishra <shresthmishr@gmail.com>
2025-08-28 13:50:37 +0530

    I meant "Complete Step 5 of Day 7"

--- Day 7 (Hangman)/Step 5.py
+++ Day 7 (Hangman)/Step 5.py
@@ -31,6 +31,7 @@ while not game_over:
     # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in correct_letters:
         print(f"You've already guessed {guess}.")
+        
     display = ""
 
     for letter in chosen_word:
