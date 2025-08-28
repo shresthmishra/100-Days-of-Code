6ae34e8ef9e02916fe31d10465a6e44ad0f95ef9
Shresth Mishra <shresthmishr@gmail.com>
2025-08-28 15:47:46 +0530

    Complete Task 2 of Day 8

--- Day 8 (Function Parameters)/Task 2.py
+++ Day 8 (Function Parameters)/Task 2.py
@@ -1,7 +1,16 @@
 # Functions with input
 
+# Positional Arguments
 def greet_with_name(name, greeting):
     print(f"{greeting}, {name}.")
 
+greet_with_name("Shresth", "Howdy") #Output: Howdy, Shresth.
 
-greet_with_name("Shresth", "Howdy")
+
+# Keyword Arguments
+
+def greet_with_location(name, location):
+    print(f"Hello there, {name}.")
+    print(f"How's it like in {location}?")
+
+greet_with_location(name="Shresth", location="Singrauli")
