acbb680d5e2eb690f36684895891fd46707176f9
Shresth Mishra <shresthmishr@gmail.com>
2026-08-20 21:43:11 +0530

    Add title

---
+++ Day 10 (Functions with Outputs)/Task_1_Functions_with_Outputs.py
@@ -0,0 +1,6 @@
+# Functions with Outputs
+'''
+We've seen functions with only an execution body, functions with inputs that allow for variation in execution of the function body. 
+Now we'll see the final form of functions. Functions that can have outputs.
+'''
+
--- Day 9 (Dictionaries & Nesting)/Task 1.py
+++ Day 9 (Dictionaries & Nesting)/Task 1.py
@@ -1,3 +1,4 @@
+# Dictionaries
 '''
 * A dictionary in Python functions similarly to a dictionary in real life. 
 * It's a data structure that allows us to associate a key to a value and pair the two pieces of data together.
